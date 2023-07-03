7dc7e138110c5c9f2972b05b8701adcc5fbbf3e0
“RafaelaSantos123” <rafaela_mam@hotmail.com>
2023-07-03 20:48:03 -0300

    Criacao estrutura do teste.

--- socialsports/src/test/java/com/example/socialsports/SocialsportsApplicationTests.java
+++ socialsports/src/test/java/com/example/socialsports/SocialsportsApplicationTests.java
@@ -2,12 +2,44 @@ package com.example.socialsports;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 @SpringBootTest
 class SocialsportsApplicationTests {
+	WebDriver driver;
+
+	@BeforeAll
+	static void setupClass() {
+		WebDriverManager.chromedriver().setup();
+	}
+
+	@BeforeEach
+	void setupTest() {
+		driver = new ChromeDriver();
+	}
+
+	@AfterEach
+	void teardown() {
+		driver.quit();
+	}
 
 	@Test
 	void contextLoads() {
+
+		// Exercise
+		driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+		String title = driver.getTitle();
+
+		// Verify
+		assertThat(title).contains("Selenium WebDriver");
 	}
 
 }
