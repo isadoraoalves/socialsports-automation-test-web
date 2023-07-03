1fb9c9920b2aaf34fe3b05bb45d6839ce6f22d9a
isadoraoalves <128647400+isadoraoalves@users.noreply.github.com>
2023-07-03 19:50:23 -0300

    Add files via upload

---
+++ socialsports/src/main/java/com/example/socialsports/SocialsportsApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialsports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialsportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialsportsApplication.class, args);
+	}
+
+}
---
+++ socialsports/src/test/java/com/example/socialsports/SocialsportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialsports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialsportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
